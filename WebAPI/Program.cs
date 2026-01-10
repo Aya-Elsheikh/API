a9f6222defbf1dc4f7f98bc9cfeea29ec27204bc
Aya Mohamed Elsheikh <ayamohamed@idsc.net.eg>
2026-01-11 00:08:01 +0200

    fix endpoint

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,8 +1,33 @@
+using Infrastructure.External;
+using MediatR;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(
+    Assembly.GetExecutingAssembly(),
+    typeof(GetConceptsHandler).Assembly
+);
+
+builder.Services.AddMediatR(
+    Assembly.GetExecutingAssembly(),
+    typeof(GetEmiratesQuery).Assembly
+);
+
+builder.Services.AddMediatR(
+    Assembly.GetExecutingAssembly(),
+    typeof(AnalyzeLocationCommand).Assembly
+);
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+builder.Services.AddSingleton<JsonReferenceDataLoader>();
+builder.Services.AddHttpClient<OverpassClient>();
+
+builder.Services.AddScoped<ICompetitionService, CompetitionService>();
+builder.Services.AddScoped<IScoringService, ScoringService>();
+builder.Services.AddHttpClient<GooglePlacesClient>();
 
 var app = builder.Build();
 
